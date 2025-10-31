11401dcb40014e08ed1fb4cdba6d089db54524d9
kyonghwa1916 - notebook <yujin0360@naver.com>
2025-10-31 10:46:32 +0900

    main.py의 game loop 기준으로 재작성

---
+++ main.py
@@ -0,0 +1,13 @@
+from pico2d import *
+import source
+
+open_canvas()
+reset_world()
+# game loop
+while running:
+    handle_events()
+    update_world()
+    render_world()
+    delay(0.01)
+# finalization code
+close_canvas()
