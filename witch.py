dbbe609f79074e7815005895266478575cd4e058
kyonghwa1916 - notebook <yujin0360@naver.com>
2025-11-03 00:16:21 +0900

    witch 내부에 인벤토리용 배열 생성

--- witch.py
+++ witch.py
@@ -38,6 +38,9 @@ class Witch:
         # 이동 속도 (픽셀/프레임)
         self.speed = 5
 
+        # 인벤토리: 15칸 고정 (None은 빈 슬롯)
+        self.inventory = [None] * 15
+
     def update(self):
         # 애니메이션 프레임 갱신
         self.frame = (self.frame + 1) % 8
@@ -57,3 +60,47 @@ class Witch:
         # 기존 소스와 동일한 clip_draw 호출을 유지합니다.
         # (left, bottom, w, h, dx, dy, dw, dh)
         self.image.clip_draw(0, self.frame * 48, 48, 48, self.x, self.y, 100, 100)
+
+    # --- Inventory helper methods ---
+    def add_to_inventory(self, item):
+        """첫 번째 빈 슬롯에 item을 넣습니다. 성공하면 인덱스를 반환하고, 가득 차 있으면 ValueError를 발생시킵니다."""
+        for i in range(len(self.inventory)):
+            if self.inventory[i] is None:
+                self.inventory[i] = item
+                return i
+        raise ValueError('Inventory is full')
+
+    def remove_from_inventory(self, index):
+        """주어진 인덱스의 아이템을 제거하고 반환합니다. 인덱스가 비어있으면 None을 반환합니다."""
+        if index < 0 or index >= len(self.inventory):
+            raise IndexError('Inventory index out of range')
+        item = self.inventory[index]
+        self.inventory[index] = None
+        return item
+
+    def swap_inventory(self, i, j):
+        """두 인덱스에 있는 아이템을 교환합니다."""
+        if not (0 <= i < len(self.inventory)) or not (0 <= j < len(self.inventory)):
+            raise IndexError('Inventory index out of range')
+        self.inventory[i], self.inventory[j] = self.inventory[j], self.inventory[i]
+
+    def get_item(self, index):
+        """인덱스에 있는 아이템을 반환(읽기 전용)."""
+        if index < 0 or index >= len(self.inventory):
+            raise IndexError('Inventory index out of range')
+        return self.inventory[index]
+
+    def inventory_summary(self):
+        """인벤토리의 요약 리스트를 반환합니다: (index, item_name_or_None)."""
+        summary = []
+        for i, it in enumerate(self.inventory):
+            if it is None:
+                summary.append((i, None))
+            else:
+                name = getattr(it, 'name', None) or getattr(it, 'filename', repr(it))
+                summary.append((i, name))
+        return summary
+
+    def has_space(self):
+        """빈 슬롯이 있는지 여부를 반환합니다."""
+        return any(x is None for x in self.inventory)
